168dca9ebbe731436891e2bdf639aac2074f10dd
Ankrutee <79003737+Ankrutee@users.noreply.github.com>
2022-11-24 17:49:54 +0530

    Update TestFail.py

--- TestFail.py
+++ TestFail.py
@@ -2,17 +2,35 @@
 # Test case for adding two numbers
 import unittest
 
-from SUbtraction import subtraction
+from Subtraction import subtraction
 
 class TestSubtract(unittest.TestCase):
-    def test_list_int(self):
+    def test_case1_int(self):
         """
         Test case to subtract two numbers
         """
         a=30
         b=30
-        result = subtraction(a,b)
-        self.assertEqual(result, 500)
+        result1 = subtraction(a,b)
+        self.assertEqual(result1, 100)
+        
+    def test_case2_int(self):
+        """
+        Test case to subtract two numbers
+        """
+        c=60
+        d=30
+        result2 = subtraction(c,d)
+        self.assertEqual(result2, 300)
+    
+    def test_case3_int(self):
+        """
+        Test case to subtract two numbers
+        """
+        e=100
+        f=40
+        result3 = subtraction(e,f)
+        self.assertEqual(result3, 600)
 
 if __name__ == '__main__':
     unittest.main()
